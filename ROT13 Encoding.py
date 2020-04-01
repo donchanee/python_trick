00af34da52ce361f18aebd75c6b4c955682829be
donchanee <dchan0305@gmail.com>
2020-04-01 11:22:47 +0900

    Create ROT13 Encoding.py

---
+++ ROT13 Encoding.py
@@ -0,0 +1,6 @@
+ROT13 is a valid encoding for source code, when you use the right coding declaration at the top of the code file:
+
+#!/usr/bin/env python
+# -*- coding: rot13 -*-
+
+cevag "Uryyb fgnpxbiresybj!".rapbqr("rot13")
